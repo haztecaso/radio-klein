14171bb040eac3119f9027ae3751e4e23c6df1bd
Adrián Lattes <adrianlattes@disroot.org>
2020-04-18 01:58:26 +0200

    Automated commit

--- assets/js/loadplayers.js
+++ assets/js/loadplayers.js
@@ -10,4 +10,31 @@ players.forEach(function(player, i){
     });
 });
 
-Amplitude.init({"songs": amplitudesongs});
+Amplitude.init({
+    "songs": amplitudesongs,
+    "volume_increment": 10,
+    "volume_decrement": 10
+});
+
+function updatevol(){
+    vol = Amplitude.getConfig().volume;
+    icons = document.querySelectorAll("span.volumeicon");
+    icons.forEach(function(icon){
+        if (vol>=50){
+            icon.classList.add("fa-volume-up");
+            icon.classList.remove("fa-volume-down");
+            icon.classList.remove("fa-volume-off");
+        }
+        else if(vol < 50 && vol > 0){
+            icon.classList.remove("fa-volume-up");
+            icon.classList.add("fa-volume-down");
+            icon.classList.remove("fa-volume-off");
+        }
+        else if(vol == 0){
+            icon.classList.remove("fa-volume-up");
+            icon.classList.remove("fa-volume-down");
+            icon.classList.add("fa-volume-off");
+        }
+    });
+}
+window.setInterval(updatevol, 500);
