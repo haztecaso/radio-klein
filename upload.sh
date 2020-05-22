78548f81f4cc1adba2eb8f7d502c2168214b83b3
Adrián Lattes <adrianlattes@disroot.org>
2020-05-22 19:24:58 +0200

    Prueba de hook

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git add .
-echo "Escribe una descripción para el commit y pulsa enter"
+echo "Escribe una descripción para el commit y pulsa enter."
 read msg
 git commit -am "$msg"
 git push
